8e4392e76891ce35cfc807078af6670c6f5f2dc1
ItsRitik <65969779+ItsRitik@users.noreply.github.com>
2024-03-08 16:39:45 -0500

    changes

--- server/server.jsx
+++ server/server.jsx
@@ -52,7 +52,7 @@ app.use((err,req,res,next)=>{
 })
 
 app.use(express.static('client/build'));
-if(process.env.NODE_ENV === 'development'){
+if(process.env.NODE_ENV === 'production'){
     const path = require('path');
     app.get('/*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
