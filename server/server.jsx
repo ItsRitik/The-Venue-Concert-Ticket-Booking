2f2e5220fbfcbb7915ba42a46d492920b3307fa7
ItsRitik <65969779+ItsRitik@users.noreply.github.com>
2024-03-09 04:42:06 -0500

    make it better

--- server/server.jsx
+++ server/server.jsx
@@ -54,13 +54,13 @@ app.use((err,req,res,next)=>{
     handleError(err,res)
 })
 
-// app.use(express.static('client/build'));
-// if(process.env.NODE_ENV === 'production'){
-//     const path = require('path');
-//     app.get('/*',(req,res)=>{
-//         res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
-//     });
-// }
+app.use(express.static('client/build'));
+if(process.env.NODE_ENV === 'production'){
+    const path = require('path');
+    app.get('/*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
+    });
+}
 
 
 const port = process.env.PORT || 3001
